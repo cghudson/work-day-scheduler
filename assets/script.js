30527e5b8b5d7b0ca97e7310accab3a5ac86f8b3
Carolyn <cghudson@uwalumni.com>
2022-03-31 19:35:22 -0500

    add js, current date function

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+//display current date on page load
+var updateDate = function () {
+    document.getElementById("currentDay")
+    .innerHTML = moment().format("MMMM Do YYYY, h:mm:ss a")
+}
+setInterval(updateDate, 1000);
+
+
+
+//display time blocks for standard business hours
+//color code times blocks - past, present, future
+
+//when timeblock clidked, enter event info
+//click save button to save to local storate
+// data from event persists when page refreshed
