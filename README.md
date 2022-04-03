00f97dda3aee059c97a8f0559982d85f2584db5d
Carolyn <cghudson@uwalumni.com>
2022-04-03 12:17:45 -0500

    add clear schedule button and made size responsive

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ---
 
 ## Description:
-This application allows users to create a schedule using the daily planner. The current date and time is displayed at the top of the page. The user is presented with timeblocks for standard business hours that are color-coded to indicate whether it is in the past, present, or future. The user can enter event details into the timeblock and save to local storage using the save button. The saved events persist when the page is refreshed. 
+This application allows users to create a schedule using the daily planner. The current date and time is displayed at the top of the page. The user is presented with timeblocks for standard business hours that are color-coded to indicate whether it is in the past, present, or future. The user can enter event details into the text area and save to local storage using the save button. The saved events persist when the page is refreshed. 
 
 ---
 
--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,57 @@
 // display current date on page load
 var updateDate = function () {
-    document.getElementById("currentDay")
-    .innerHTML = moment().format("MMMM Do YYYY, h:mm:ss a")
-}
+  document.getElementById("currentDay").innerHTML = moment().format(
+    "MMMM Do YYYY, h:mm:ss a"
+  );
+};
 setInterval(updateDate, 1000);
 
-$(document).ready(function() {
-    //assign past, present, or future to timeblocks
-    function scheduleTime() {
-        var hour = moment().hours()
-        
-        $(".time-block").each(function() {
-            var workTime = $(this).attr("id")
+//assign past, present, or future to timeblocks
+function scheduleTime() {
+  var hour = moment().hours();
 
-            if (workTime < hour) {
-                // $(this).removeClass("future")
-                // $(this).removeClass("present")
-                $(this).addClass("past")
-            } else if (workTime == hour) {
-                // $(this).removeClass("future")
-                $(this).removeClass("past")
-                $(this).addClass("present")
-            } else {
-                $(this).removeClass("present")
-                // $(this).removeClass("past")
-                $(this).addClass("future")
-            }
-        })
+  $(".time-block").each(function () {
+    var workTime = $(this).attr("id");
+
+    if (workTime < hour) {
+      $(this).addClass("past");
+    } else if (workTime == hour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("present");
+      $(this).addClass("future");
     }
-    
-    //save to local storage
-    $(".saveBtn").click(function() {
-        var time = $(this).parent().attr("id")
-        var task = $(this).siblings(".description").val()
-        
-        localStorage.setItem(time, task)
-    })
+  });
+}
 
-    scheduleTime()
-})
+//save to local storage
+$(".saveBtn").click(function () {
+  var time = $(this).parent().attr("id");
+  var task = $(this).siblings(".description").val();
+
+  localStorage.setItem(time, task);
+});
+
+scheduleTime();
 
 // data from event persists when page refreshed
-$("#9 .description").val(localStorage.getItem("9"))
-$("#10 .description").val(localStorage.getItem("10"))
-$("#11 .description").val(localStorage.getItem("11"))
-$("#12 .description").val(localStorage.getItem("12"))
-$("#13 .description").val(localStorage.getItem("13"))
-$("#14 .description").val(localStorage.getItem("14"))
-$("#15 .description").val(localStorage.getItem("15"))
-$("#16 .description").val(localStorage.getItem("16"))
-$("#17 .description").val(localStorage.getItem("17"))
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+//clear schedule
+function clearSchedule() {
+    $(".clear").click(function() {
+    localStorage.clear()
+    location.reload()
+})
+}
+clearSchedule()
+
